4ac14b156671a836b5eb0726c3e9546f7df00a73
Manuel Clairice <manuel.clairice@gmail.com>
2022-01-14 00:11:10 +0100

    Make some modification before final version

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 Creating a Command Line Interface that scrapes a memes website
+
+Creating a request
+
+Process the HTML string to create an array of image URLs
+
+Search the website HTML for the src of the img tags (this will be the URL of the image)
+
+Loop over the array of image URLs
--- index.js
+++ index.js
@@ -1,14 +1,23 @@
-// Process the HTML string to create an array of image URL with (cheerio)
+import axios from 'axios';
 import cheerio from 'cheerio';
-// creating the request from the website ( requestify )
-import requestify from 'requestify';
+import fs from 'fs';
 
-requestify
+axios
   .get('https://memegen-link-examples-upleveled.netlify.app/')
-  .then(function (response) {
-    response.getBody();
+  .then((response) => {
+    let $ = cheerio.load(response.data);
+    let articles = [];
+
+    $('.data-new-gr-c-s-check-loaded').each((index, element) => {
+      articles.push({
+        title: $(element).find('img').text().trim(),
+      });
+    });
+    // saving the picture in the memes file
+    fs.writeFile('./memes', JSON.stringify(articles), (error) => {
+      if (error) throw error;
+    });
+  })
+  .catch((error) => {
+    console.log(error);
   });
-// trying the code from cheerio to see if it comes back with error
-const imageUrl = cheerio.load(
-  '<img src="https://api.memegen.link/images/mordor/one_does_not_simply/walk_into_mordor.jpg?width=300">',
-);
