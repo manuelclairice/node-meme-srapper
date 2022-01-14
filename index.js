e6aa7a268fd5696f2336c5c4bb859a6c4c8729d3
Manuel Clairice <manuel.clairice@gmail.com>
2022-01-14 01:46:56 +0100

    Try a complete different approache to complete the code

--- index.js
+++ index.js
@@ -1,23 +1,48 @@
-import axios from 'axios';
-import cheerio from 'cheerio';
-import fs from 'fs';
+import * as fs from 'fs';
+import got from 'got';
+import htmlUrls from 'html-urls';
+import fetch from 'node-fetch';
 
-axios
-  .get('https://memegen-link-examples-upleveled.netlify.app/')
-  .then((response) => {
-    let $ = cheerio.load(response.data);
-    let articles = [];
+// create the request or connect to the website
+async function memescraper() {
+  const websiteUrl = 'https://memegen-link-examples-upleveled.netlify.app/';
+  const { body: html } = await got(websiteUrl);
+  const links = htmlUrls({ html, websiteUrl });
 
-    $('.data-new-gr-c-s-check-loaded').each((index, element) => {
-      articles.push({
-        title: $(element).find('img').text().trim(),
-      });
-    });
-    // saving the picture in the memes file
-    fs.writeFile('./memes', JSON.stringify(articles), (error) => {
-      if (error) throw error;
-    });
-  })
-  .catch((error) => {
-    console.log(error);
+  const allWebsiteUrl = [];
+
+  links.forEach((element) => {
+    allWebsiteUrl.push(element.websiteUrl);
   });
+
+  // Process the HTML string to create an array of image URLs
+  const imageUrl = allWebsiteUrl.filter(
+    (element) => element.startsWith('https://api.memegen.link/images'), // narrow down to the the image Url
+  );
+
+  // Loop over the array of image URLs
+  for (let i = 0; i < 10; i++) {
+    fs.open(`./memes/0${i + 1}.jpg`, 'w', (err) => {
+      // Save the image data in folder memes with file names 01.jpg, etc
+      if (err) {
+        throw err;
+      }
+    });
+
+    // Downloading the image and creating a file
+    const savedImageUrl = imageUrl[i];
+
+    fetch(savedImageUrl)
+      .then((res) =>
+        res.body.pipe(fs.createWriteStream(`./memes/0${i + 1}.jpg`)),
+      )
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+}
+
+// Test the program by running node index.js multiple times
+memescraper().catch((error) => {
+  console.log(error);
+});
